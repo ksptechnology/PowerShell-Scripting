c80f1b5ecae599aa188d225c1b3cac698af5dc25
eashdown-sudo <eashdown@ksp.ca>
2020-01-09 11:40:30 -0600

    Adds Documentation for O365 Folder

---
+++ O365 Junk Folder/Disable Junk Folder - 365.ps1
@@ -0,0 +1,17 @@
+﻿$UserCredential = Get-Credential -Credential ksp365admin@
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking -Prefix 365
+#Any commands in 365 need a "365" prefix. ie: get-365mailboxfolderpermission
+
+#for all users:
+#Get-365mailbox | Set-365MailboxJunkEmailConfiguration –Enabled $False
+
+#for one user/shared mailbox:
+#Set-365MailboxJunkEmailConfiguration accounting@scsaonline.ca –Enabled $False
+
+#Check Junk folder status once run:
+#Get-365MailboxJunkEmailConfiguration accounting@scsaonline.ca
+
+<#When Complete: 
+Remove-pssession $session
+#>
---
+++ O365 Junk Folder/README.md
@@ -0,0 +1,3 @@
+#Disable Junk Folder - 365
+Script for disabling the Junk folder for all mailboxes in a 365 tenant.
+To be run against every new 365 tenant as Junk folder is not needed.
